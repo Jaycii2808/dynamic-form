57913bf54381e64376be4bc920d92c3847fad2d3
Chau Dinh Thong <dinhthongchau@gmail.com>
2025-07-14 09:50:22 +0700

    Refactor: move dynamic_form_multi_model to its own folder
    The `dynamic_form_multi_model.dart` file has been moved from `lib/data/models/` to `lib/data/models/dynamic_form_multiline/`.
    
    Imports have been updated accordingly in the following files:
    - `lib/presentation/screens/saved_forms_screen.dart`
    - `lib/presentation/bloc/multi_page_form/multi_page_form_bloc.dart`
    - `lib/presentation/bloc/multi_page_form/multi_page_form_state.dart`
    - `lib/presentation/widgets/dynamic_form_multi_page_widget.dart`

--- lib/presentation/bloc/multi_page_form/multi_page_form_state.dart
+++ lib/presentation/bloc/multi_page_form/multi_page_form_state.dart
@@ -1,4 +1,4 @@
-import 'package:dynamic_form_bi/data/models/dynamic_form_multi_model.dart';
+import 'package:dynamic_form_bi/data/models/dynamic_form_multiline/dynamic_form_multi_model.dart';
 import 'package:equatable/equatable.dart';
 
 abstract class MultiPageFormState extends Equatable {
--- lib/presentation/screens/saved_forms_screen.dart
+++ lib/presentation/screens/saved_forms_screen.dart
@@ -6,7 +6,7 @@ import 'package:dynamic_form_bi/presentation/screens/form_preview_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:dynamic_form_bi/core/enums/date_picker_enum.dart';
-import 'package:dynamic_form_bi/data/models/dynamic_form_multi_model.dart';
+import 'package:dynamic_form_bi/data/models/dynamic_form_multiline/dynamic_form_multi_model.dart';
 import 'package:dynamic_form_bi/presentation/screens/preview_multipage_screen.dart';
 import 'package:flutter/services.dart';
 import 'dart:convert';
